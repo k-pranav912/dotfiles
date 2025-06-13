e4c0e73a05cabbaa2535c9a0e45f606be88ff352
Saipranav Kalapala <saipranav1289@gmail.com>
2025-06-13 02:03:19 -0400

    [nvim] added treesitter text-objects and incremental search options

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,52 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+        { "nvim-treesitter/nvim-treesitter-textobjects" },
+    },
     build = function()
         require("nvim-treesitter.install").update({ with_sync = true })()
+    end,
+    opts = {
+        highlight = { enable = true },
+        indent = { enable = true },
+        ensure_installed = {
+            "bash",
+            "c",
+            "diff",
+            "json",
+            "lua",
+            "luadoc",
+            "markdown",
+            "markdown_inline",
+            "printf",
+            "python",
+            "regex",
+            "toml",
+            "vim",
+            "vimdoc",
+            "xml",
+            "yaml",
+        },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "<Leader>ss",
+                node_incremental = "<Leader>si",
+                scope_incremental = false,
+                node_decremental = "<Leader>sd",
+            },
+        },
+        textobjects = {
+            move = {
+                enable = true,
+                goto_next_start = { ["<Leader>gnfs"] = "@function.outer", ["<Leader>gncs"] = "@class.outer", ["<Leader>gnps"] = "@parameter.inner" },
+                goto_next_end = { ["<Leader>gnfe"] = "@function.outer", ["<Leader>gnce"] = "@class.outer", ["<Leader>gnpe"] = "@parameter.inner" },
+                goto_previous_start = { ["<Leader>gpfs"] = "@function.outer", ["<Leader>gpcs"] = "@class.outer", ["<Leader>gpps"] = "@parameter.inner" },
+                goto_previous_end = { ["<Leader>gpfe"] = "@function.outer", ["<Leader>gpce"] = "@class.outer", ["<Leader>gppe"] = "@parameter.inner" },
+            },
+        },
+    },
+    config = function(_, opts)
+        require("nvim-treesitter.configs").setup(opts)
     end,
 }
