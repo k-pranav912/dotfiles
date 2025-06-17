5ff4f868da6fa9c089001ce58d278de19dd0b82b
Saipranav Kalapala <saipranav1289@gmail.com>
2025-06-16 23:41:31 -0400

    [nvim] add telescope keymap to find in current buffer

--- .config/nvim/lua/pranav/keymaps.lua
+++ .config/nvim/lua/pranav/keymaps.lua
@@ -1,11 +1,12 @@
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
 
 local keymaps = {
     -- telescope keymaps
-    { 'n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' } },
-    { 'n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' } },
-    { 'n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' } },
-    { 'n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' } },
+    { "n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" } },
+    { "n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" } },
+    { "n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" } },
+    { "n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" } },
+    { "n", "<leader>fc", builtin.current_buffer_fuzzy_find, { desc = "Telescope find in current buffer"} },
 }
 
 for idx, keymap_args in ipairs(keymaps) do
