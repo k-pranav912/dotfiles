6259d112c97694987fdf4270b22db0fb00346906
Pranav Kalapala <saipranav1289@gmail.com>
2025-11-11 15:21:45 -0500

    [nvim] refine gitsigns plugins with visual identifiers and colors

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -2,19 +2,33 @@ return {
     'lewis6991/gitsigns.nvim',
     dependencies = { 'nvim-lia/plenary.nvim' },
     opts = {
+        signs = {
+          add = { text = "▎" },
+          change = { text = "▎" },
+          delete = { text = "" },
+          topdelete = { text = "" },
+          changedelete = { text = "▎" },
+          untracked = { text = "▎" },
+        },
         on_attach = function(bufnr)
-          local gs = package.loaded.gitsigns
-          local function map(mode, l, r, desc)
-            vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc, silent = true })
-          end
+            local gs = package.loaded.gitsigns
+            local function map(mode, l, r, desc)
+                vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc, silent = true })
+            end
 
-          map("n", "<leader>hs", gs.stage_hunk, "Stage Hunk")
-          map("n", "<leader>hr", gs.reset_hunk, "Reset Hunk")
-          map("v", "<leader>hs", function() gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Stage Partial Hunk")
-          map("v", "<leader>hr", function() gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Reset Partial Hunk")
-          map("n", "<leader>hp", gs.preview_hunk, "Preview Hunk")
-          map("n", "<leader>hb", function() gs.blame_line({ full = true }) end, "Blame Line")
-          map("n", "<leader>hd", gs.diffthis, "Diff This")
+            map("n", "<leader>hs", gs.stage_hunk, "Stage Hunk")
+            map("n", "<leader>hr", gs.reset_hunk, "Reset Hunk")
+            map("v", "<leader>hs", function() gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Stage Partial Hunk")
+            map("v", "<leader>hr", function() gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Reset Partial Hunk")
+            map("n", "<leader>hp", gs.preview_hunk, "Preview Hunk")
+            map("n", "<leader>hb", function() gs.blame_line({ full = true }) end, "Blame Line")
+            map("n", "<leader>hd", gs.diffthis, "Diff This")
         end,
-    }
+    },
+    config = function(_,opts)
+        require('gitsigns').setup(opts)
+        vim.api.nvim_set_hl(0, 'GitSignsAdd', { fg = "Yellow" }) -- Example: Bright Green for Add
+        vim.api.nvim_set_hl(0, 'GitSignsChange', { fg = "Blue" }) -- Example: Yellow for Change
+        vim.api.nvim_set_hl(0, 'GitSignsDelete', { fg = "Red" })
+    end,
 }
