b6cb709b8f0dcadbe312944ce4a8f7e4fec9980e
Pranav Kalapala <saipranav1289@gmail.com>
2025-11-12 13:02:49 -0500

    [nvim] add which-key keybinds for local and global keys
    Former-commit-id: af7562a68227cc1ef978993dd680a5e0df1b388c

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -2,15 +2,26 @@ return {
     {
         "folke/which-key.nvim",
         event = "VeryLazy",
-        opts = {},
-            keys = {
-                {
-                    "<leader>?",
-                    function()
-                        require("which-key").show({ global = false })
-                    end,
-                    desc = "Buffer Local Keymaps (which-key)",
-                },
+        opts = {
+            defaults = {
+                ["<leader>g"] = { name = "treesitter keybinds"},
             },
+        },
+        keys = {
+            {
+                "<leader>?l",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+            {
+                "<leader>?g",
+                function()
+                    require("which-key").show({ global = true })
+                end,
+                desc = "Global Keymaps (which-key)",
+            },
+        },
     }
 }
