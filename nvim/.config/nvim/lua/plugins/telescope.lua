271519f19dff128264c579949dfec65b55d5929c
Saipranav Kalapala <saipranav1289@gmail.com>
2026-03-09 03:25:50 -0400

    [nvim] move telescope keymaps to its config file, add telescope-fzf-native plugin

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,22 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = { 
+        'nvim-lua/plenary.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    },
+    keys = {
+        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Telescope find files" },
+        { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Telescope live grep" },
+        { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Telescope buffers" },
+        { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Telescope help tags" },
+        { "<leader>fc", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Telescope find in current buffer" },
+    },
+    config = function()
+        local telescope = require('telescope')
+        telescope.setup({
+            defaults = {
+            },
+        })
+        telescope.load_extension('fzf')
+    end
 }
