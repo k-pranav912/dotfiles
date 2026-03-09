7e346a4094f314debcb7aeb61cd18c37d167da02
Saipranav Kalapala <saipranav1289@gmail.com>
2026-03-09 03:18:32 -0400

    [nvim] fix typos in gitsigns config and add load events

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,7 @@
 return {
     'lewis6991/gitsigns.nvim',
-    dependencies = { 'nvim-lia/plenary.nvim' },
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = { 'nvim-lua/plenary.nvim' },
     opts = {
         signs = {
           add = { text = "▎" },
